89fdfaf9f9e1a956b1515e80e7ff25306d6fb786
afdul <afdul100ahmad@gmail.com>
2025-04-05 23:26:06 +0530

    Prometheus fix

--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,35 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import AsyncSessionLocal
 from app import crud, schemas
-from fastapi.responses import FileResponse , StreamingResponse
+from fastapi.responses import FileResponse , StreamingResponse , PlainTextResponse
 import aiohttp
 import httpx
 from aiohttp import ClientSession
 from fastapi.responses import StreamingResponse
+from prometheus_client import Counter
 
 router = APIRouter()
+stream_counter = Counter(
+    "stream_requests_total", 
+    "Total stream requests",
+    ["clip_id"]
+)
 
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
+
+@router.get("/", response_class=PlainTextResponse)
+def root():
+    return """API Endpoints
+Method    Endpoint                       Description
+GET       /clips                        List all available audio clips
+POST      /clips                        Create a new audio clip entry
+GET       /clips/{clip_id}/stream       Stream the audio clip
+GET       /clips/{clip_id}/stats        Get play count and clip metadata
+GET       /metrics                      Prometheus metrics export
+"""
+
 
 @router.get("/clips", response_model=list[schemas.ClipOut])
 async def list_clips(db: AsyncSession = Depends(get_db)):
@@ -30,6 +48,9 @@ async def stream_clip(clip_id: int, db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Clip not found")
 
     await crud.increment_play_count(db, clip)
+    stream_counter.labels(clip_id=str(clip_id)).inc()
+
+
 
     async def proxy_stream():
         async with ClientSession() as session:
